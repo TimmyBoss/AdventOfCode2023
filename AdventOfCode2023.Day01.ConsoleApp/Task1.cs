9d803f1926c6244e061acd8c6215e4c785ccd41a
Tim Harte <tim@scorito.com>
2023-12-01 15:40:01 +0100

    Day1 task2 wip

--- AdventOfCode2023.Day01.ConsoleApp/Task1.cs
+++ AdventOfCode2023.Day01.ConsoleApp/Task1.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode2023.Shared;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2023.Day01.ConsoleApp
 {
@@ -12,9 +13,11 @@ namespace AdventOfCode2023.Day01.ConsoleApp
             foreach (string row in _puzzleInput)
             {
                 List<char> numbers = row.Where(char.IsDigit).ToList();
-                var number = numbers[0].ToString() + numbers[numbers.Count - 1].ToString();
-
-                total += Convert.ToInt32(number);
+                if (numbers.Count > 0)
+                {
+                    var number = numbers[0].ToString() + numbers[numbers.Count - 1].ToString();
+                    total += Convert.ToInt32(number);
+                }
             }
 
             return total.ToString();
@@ -22,9 +25,38 @@ namespace AdventOfCode2023.Day01.ConsoleApp
 
         public string GetAnswer2()
         {
-            //   var sorted = _totalCaloriesList.OrderByDescending(t => t).Take(3);
-            //     return sorted.Sum().ToString();
-            return "Henk";
+            int total = 0;
+
+            foreach (string row in _puzzleInput)
+            {
+                var numbers = Regex.Matches(row, @"one|two|three|four|five|six|seven|eight|nine|\d")
+                    .Select(m => m.Value).ToArray();
+
+                var number = ReplaceWithDigit(numbers[0]) + ReplaceWithDigit(numbers[numbers.Length - 1]);
+
+                //    Console.WriteLine(row + "  --  " + number);
+
+                total += Convert.ToInt32(number);
+            }
+
+            return total.ToString();
+        }
+
+        private string ReplaceWithDigit(string value)
+        {
+            switch (value)
+            {
+                case "one": return "1";
+                case "two": return "2";
+                case "three": return "3";
+                case "four": return "4";
+                case "five": return "5";
+                case "six": return "6";
+                case "seven": return "7";
+                case "eight": return "8";
+                case "nine": return "9";
+                default: return value;
+            }
         }
 
         public void SetupPuzzleInput(List<string> puzzleInput)
