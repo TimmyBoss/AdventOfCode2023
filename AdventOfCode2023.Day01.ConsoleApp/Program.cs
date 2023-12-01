6abd8ecd3f079db979197d39b64f299587241153
Tim Harte <tim@scorito.com>
2023-12-01 10:07:34 +0100

    Solved day 1 - task 1

---
+++ AdventOfCode2023.Day01.ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2023.Day01.ConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+var task = new AdventOfCode2023.Shared.Task<Task1>();
+task.ReadAnswers();
---
+++ AdventOfCode2023.Day01.ConsoleApp/Task1.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode2023.Shared;
+
+namespace AdventOfCode2023.Day01.ConsoleApp
+{
+    public class Task1 : ITask
+    {
+        private List<string> _puzzleInput = new List<string>();
+
+        public string GetAnswer1()
+        {
+            long total = 0;
+            foreach (string row in _puzzleInput)
+            {
+                List<char> numbers = row.Where(char.IsDigit).ToList();
+                var number = numbers[0].ToString() + numbers[numbers.Count - 1].ToString();
+
+                total += Convert.ToInt32(number);
+            }
+
+            return total.ToString();
+        }
+
+        public string GetAnswer2()
+        {
+            //   var sorted = _totalCaloriesList.OrderByDescending(t => t).Take(3);
+            //     return sorted.Sum().ToString();
+            return "Henk";
+        }
+
+        public void SetupPuzzleInput(List<string> puzzleInput)
+        {
+            _puzzleInput = puzzleInput;
+        }
+    }
+}
