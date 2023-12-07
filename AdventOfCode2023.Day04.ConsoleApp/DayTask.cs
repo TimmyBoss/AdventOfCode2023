1adcb9d32682be4ccb7841a265980c5034753ff2
Tim Harte <tim@scorito.com>
2023-12-07 19:16:38 +0100

    Fixed task 1 day 4

---
+++ AdventOfCode2023.Day04.ConsoleApp/DayTask.cs
@@ -0,0 +1,47 @@
+﻿using AdventOfCode2023.Shared;
+
+namespace AdventOfCode2023.Day04.ConsoleApp
+{
+    internal class DayTask : ITask
+    {
+        private readonly List<ScratchCard> _scratchCards = new List<ScratchCard>();
+
+        public string GetAnswer1()
+        {
+            var totalPoints = 0;
+
+            foreach (var card in _scratchCards)
+            {
+                var cardPoints = 0;
+
+                foreach (var userNumber in card.UserNumbers)
+                {
+                    if (card.WinningNumbers.Contains(userNumber))
+                    {
+                        if (cardPoints >= 1)
+                            cardPoints *= 2;
+                        else
+                            cardPoints++;
+                    }
+                }
+
+                totalPoints += cardPoints;
+            }
+
+            return totalPoints.ToString();
+        }
+
+        public string GetAnswer2()
+        {
+            return "";
+        }
+
+        public void SetupPuzzleInput(List<string> puzzleInput)
+        {
+            foreach (var input in puzzleInput)
+            {
+                _scratchCards.Add(new ScratchCard(input));
+            }
+        }
+    }
+}
---
+++ AdventOfCode2023.Day04.ConsoleApp/ScratchCard.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode2023.Day04.ConsoleApp
+{
+    public class ScratchCard
+    {
+        public ScratchCard(string input)
+        {
+            string[] inputParts = input.Split(':');
+            CardNumber = GetCardNumber(inputParts[0]);
+
+            string[] inputNumberParts = inputParts[1].Split("|");
+            WinningNumbers = GetNumberList(inputNumberParts[0]);
+            UserNumbers = GetNumberList(inputNumberParts[1]);
+        }
+
+        public int CardNumber { get; set; }
+        public List<int> WinningNumbers { get; set; }
+        public List<int> UserNumbers { get; set; }
+
+        public int GetCardNumber(string input)
+        {
+            int cardNumber = Convert.ToInt32(input.Replace("Card ", ""));
+            return cardNumber;
+        }
+
+        public List<int> GetNumberList(string input)
+        {
+            var numbers = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var numberList = new List<int>();
+
+            foreach (var number in numbers)
+                numberList.Add(Convert.ToInt32(number));
+
+            return numberList;
+        }
+    }
+}
